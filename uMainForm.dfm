f48cf0b9059c9ae0cbc6f55f5684d95551f04d16
Sergey Aganin <sergey.aganin@gmail.com>
2012-01-31 17:33:41 +1000

    Первая тестовая версия.

--- uMainForm.dfm
+++ uMainForm.dfm
@@ -2,16 +2,65 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 216
-  ClientWidth = 426
+  ClientHeight = 412
+  ClientWidth = 550
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  Menu = mmMain
   OldCreateOrder = False
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object reAds: TRichEdit
+    Left = 0
+    Top = 0
+    Width = 550
+    Height = 412
+    Align = alClient
+    Font.Charset = RUSSIAN_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    ReadOnly = True
+    ScrollBars = ssBoth
+    TabOrder = 0
+  end
+  object amMain: TActionManager
+    Left = 512
+    Top = 40
+    StyleName = 'Platform Default'
+    object aExit: TAction
+      Caption = #1042#1099#1093#1086#1076
+      ShortCut = 27
+      OnExecute = aExitExecute
+    end
+    object aLoad: TAction
+      Caption = #1047#1072#1075#1088#1091#1079#1082#1072
+      OnExecute = aLoadExecute
+    end
+  end
+  object mmMain: TMainMenu
+    Left = 472
+    Top = 40
+    object miFile: TMenuItem
+      Caption = #1060#1072#1081#1083
+      object miLoad: TMenuItem
+        Action = aLoad
+      end
+      object miExit: TMenuItem
+        Action = aExit
+      end
+    end
+  end
+  object odMain: TOpenDialog
+    Filter = #1044#1086#1082#1091#1084#1077#1085#1090' Microsoft Word|*.doc;*.docx'
+    Left = 512
+    Top = 88
+  end
 end
--- uMainForm.pas
+++ uMainForm.pas
@@ -4,11 +4,24 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus,
+  Vcl.ActnList, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.PlatformDefaultStyleActnCtrls,
+  Vcl.ActnMan, Vcl.ExtDlgs;
 
 type
   TForm1 = class( TForm )
-    procedure FormCreate( Sender: TObject );
+    amMain: TActionManager;
+    reAds: TRichEdit;
+    aExit: TAction;
+    aLoad: TAction;
+    mmMain: TMainMenu;
+    miFile: TMenuItem;
+    miLoad: TMenuItem;
+    miExit: TMenuItem;
+    odMain: TOpenDialog;
+    procedure FormCreate(Sender: TObject);
+    procedure aLoadExecute(Sender: TObject);
+    procedure aExitExecute(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,18 +37,53 @@ implementation
 
 uses uHandlerAds;
 
-procedure TForm1.FormCreate( Sender: TObject );
-var
-  Test: THandlerAds;
+procedure TForm1.FormCreate(Sender: TObject);
 begin
-  Test := NIL;
-  try
-    Test := THandlerAds.Create();
-    Test.Load( 'c:\temp\doc1.docx' );
-  finally
-    if Assigned( Test ) then
-      Test.Free();
+//
+end;
+
+procedure TForm1.aLoadExecute(Sender: TObject);
+var
+  HandlerAds: THandlerAds;
+  AdIndex,
+  TelIndex,
+  OldSelStart: Integer;
+begin
+  if odMain.Execute() then
+  begin
+    HandlerAds := NIL;
+    try
+      HandlerAds := THandlerAds.Create();
+      OldSelStart := 0;
+      if HandlerAds.Load( odMain.FileName ) then
+      begin
+        for AdIndex := 0 to HandlerAds.AdsCount - 1 do
+          reAds.Lines.Add( Format( '%d. %s',
+            [AdIndex + 1, HandlerAds.Ads[AdIndex].Text + sLineBreak] ) );
+        for AdIndex := 0 to HandlerAds.AdsCount - 1 do
+          for TelIndex := 0 to HandlerAds.Ads[AdIndex].TelephonesCount - 1 do
+          begin
+            reAds.SelStart := reAds.FindText(
+              HandlerAds.Ads[AdIndex].Telephones[TelIndex], OldSelStart,
+              Length( reAds.Text ), [] );
+            reAds.SelLength :=
+              Length( HandlerAds.Ads[AdIndex].Telephones[TelIndex] );
+            reAds.SelAttributes.Color := clRed;
+            reAds.SelAttributes.Style := [fsBold];
+            OldSelStart := reAds.SelStart + reAds.SelLength;
+          end;
+      end;
+      reAds.SelLength := 0;
+    finally
+      if Assigned( HandlerAds ) then
+        HandlerAds.Free();
+    end;
   end;
+end;
+
+procedure TForm1.aExitExecute(Sender: TObject);
+begin
+  Close();
 end;
 
 end.
